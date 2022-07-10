ea9d2137b0c585759ebb5f6250f506029f058920
SkullLunar <merlijndeheus@outlook.com>
2022-07-10 12:43:45 +0200

    Added particle toggle system test

--- Assets/TriggerScript.cs
+++ Assets/TriggerScript.cs
@@ -7,10 +7,21 @@ public class TriggerScript : MonoBehaviour
     public GameObject thePlayer;
     public GameObject cutsceneCam;
 
+    public GameObject fire1;
+    public GameObject fire2;
+    public GameObject fire3;
+    public GameObject fire4;
+    public GameObject fire5;
+    public GameObject fire6;
+    public GameObject fire7;
+    public GameObject fire8;
+
     void OnTriggerEnter(Collider col)
 {   
     cutsceneCam.SetActive(true);
     thePlayer.SetActive(false);
+
+    fire1.SetActive(false);
     Debug.Log("COLLIDED");
 
 }
