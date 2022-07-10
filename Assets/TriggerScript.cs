35f99f4b01a80c80af610f8931bbd2988d168246
SkullLunar <merlijndeheus@outlook.com>
2022-07-10 19:28:42 +0200

    Added Time Delay/Switch back to main menu

--- Assets/TriggerScript.cs
+++ Assets/TriggerScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerScript : MonoBehaviour
 {   
@@ -18,13 +19,19 @@ public class TriggerScript : MonoBehaviour
     cutsceneCam.SetActive(true);
     thePlayer.SetActive(false);
 
-    //play Audio
-    source.PlayOneShot(clip);
-
     //Particle system activation
     fire.SetActive(true);
 
     //Debug collission check
     Debug.Log("COLLIDED");
+
+    StartCoroutine(TimeDelay());
 }
+
+IEnumerator TimeDelay()
+{
+yield return new WaitForSeconds(5);
+ SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+}
+
 }
