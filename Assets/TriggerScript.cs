79c939c82fae861dd27d7891445168ac95f05987
SkullLunar <merlijndeheus@outlook.com>
2022-07-10 14:08:01 +0200

    Finished particle activation

--- Assets/TriggerScript.cs
+++ Assets/TriggerScript.cs
@@ -7,14 +7,7 @@ public class TriggerScript : MonoBehaviour
     public GameObject thePlayer;
     public GameObject cutsceneCam;
 
-    public GameObject fire1;
-    public GameObject fire2;
-    public GameObject fire3;
-    public GameObject fire4;
-    public GameObject fire5;
-    public GameObject fire6;
-    public GameObject fire7;
-    public GameObject fire8;
+    public GameObject fire;
 
     void OnTriggerEnter(Collider col)
 {   
@@ -23,14 +16,7 @@ public class TriggerScript : MonoBehaviour
     thePlayer.SetActive(false);
 
     //Particle system activation
-    fire1.SetActive(true);
-    fire2.SetActive(true);
-    fire3.SetActive(true);
-    fire4.SetActive(true);
-    fire5.SetActive(true);
-    fire6.SetActive(true);
-    fire7.SetActive(true);
-    fire8.SetActive(true);
+    fire.SetActive(true);
 
     //Debug collission check
     Debug.Log("COLLIDED");
