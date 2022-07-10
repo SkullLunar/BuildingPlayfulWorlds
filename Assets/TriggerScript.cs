40728b5aa73f4269663246b979855a29bca87a08
SkullLunar <merlijndeheus@outlook.com>
2022-07-10 07:43:30 +0200

    Revert "Failed version of particle system"
    This reverts commit c532911e462d6f4dea948a850a4ce0c4e8809f0a.

--- Assets/TriggerScript.cs
+++ Assets/TriggerScript.cs
@@ -4,24 +4,8 @@ using UnityEngine;
 
 public class TriggerScript : MonoBehaviour
 {
-    [SerializeField] ParticleSystem fireParticle1 = null;
-    [SerializeField] ParticleSystem fireParticle2 = null;
-    [SerializeField] ParticleSystem fireParticle3 = null;
-    [SerializeField] ParticleSystem fireParticle4 = null;
-    [SerializeField] ParticleSystem fireParticle5 = null;
-    [SerializeField] ParticleSystem fireParticle6 = null;
-    [SerializeField] ParticleSystem fireParticle7 = null;
-    [SerializeField] ParticleSystem fireParticle8 = null;
     void OnTriggerEnter(Collider col)
 {
     Debug.Log("COLLIDED");
-      fireParticle1.Play();
-      fireParticle2.Play();
-      fireParticle3.Play();
-      fireParticle4.Play();
-      fireParticle5.Play();
-      fireParticle6.Play();
-      fireParticle7.Play();
-      fireParticle8.Play();
 }
 }
