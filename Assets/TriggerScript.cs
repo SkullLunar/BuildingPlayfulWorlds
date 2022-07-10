27c26612ec0e40533b455fc9cfdafefdf6f11963
SkullLunar <merlijndeheus@outlook.com>
2022-07-10 07:56:50 +0200

    Added Camera Switch/Cutscene

--- Assets/TriggerScript.cs
+++ Assets/TriggerScript.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerScript : MonoBehaviour
-{
+{   
+    public GameObject thePlayer;
+    public GameObject cutsceneCam;
+
     void OnTriggerEnter(Collider col)
-{
+{   
+    cutsceneCam.SetActive(true);
+    thePlayer.SetActive(false);
     Debug.Log("COLLIDED");
+
 }
 }
